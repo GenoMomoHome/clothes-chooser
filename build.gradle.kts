5b3ccfdaedcd81f9c819103cd6a0d659b6cbe314
Y. Z. Chen <754097987@qq.com>
2024-04-06 20:48:21 +0800

    [Update] start writing clothes-chooser, but I have no idea.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     kotlin("jvm")
+    application
+}
+
+dependencies {
+    implementation(project(":db"))
+}
+
+application {
+    mainClass = "com.geno1024.home.tools.clotheschooser.Main"
 }
---
+++ src/main/kotlin/com/geno1024/home/tools/clotheschooser/Main.kt
@@ -0,0 +1,10 @@
+package com.geno1024.home.tools.clotheschooser
+
+object Main
+{
+    @JvmStatic
+    fun main(args: Array<String>)
+    {
+
+    }
+}
